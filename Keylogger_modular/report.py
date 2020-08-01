c39632e92ef8c2e79b503bd4de3bcd392841f944
JulioEscalon <59298247+JulioEscalon@users.noreply.github.com>
2020-08-01 04:37:18 -0500

    Update report.py
    Agregadas las funciones para mostrar el reporte de la arquitectura del computador, el tiempo, y la captura de las oraciones, aún no implementado

--- Keylogger_modular/report.py
+++ Keylogger_modular/report.py
@@ -1,21 +1,43 @@
-from datetime import datetime
+from os.path import isfile
+import platform
+import os
 
 
 class report:
-    z = datetime.now()
-    m = "keylogger_fecha_{}.txt".format(z.strftime("%d-%m-%Y_%H-%M-%S"))
      
-    #inicializa variable para crear txt
-    def __init__(self, rarch, rtiming, rcount):
-        self.rarch = rarch
-        self.rtiming = rtiming
-        self.rcount = rcount
+    def __init__(self, nombre):
+        self.nombre=nombre
 
-    #crea el txt
-    def crearTXT(self,captexto):
-        archivo = open(self.m,"w+")
+    def logging(self,captexto):
+        archivo = open(self.nombre,"a")
         archivo.write(captexto)
         archivo.close()
 
+    def timing(self,captured,nombre):
+        if isfile(nombre):
+            archivo = open(nombre,"a")
+            archivo.write(captured+"\n")
+            archivo.close()
+        else:
+            archivo = open(nombre,"w")
+            archivo.write("Tecla\t\t\t\tInicio\t\t\t\t\tFin\n")
+            archivo.close()
+
+    def arch(self):
+        so=platform.system()
+        if so=='Windows':
+            command="systeminfo > "+self.nombre
+            os.system(command)
+        elif so=='Linux':
+            command="lscpu | head -24 | tee "+self.nombre
+            os.system(command)
     
-    
+    def joinFiles(self,file1,file2,newReport):
+        txt1 = open(file1,"r").read()
+        txt2 = open(file2,"r").read()
+        #Invocar al nuevo file
+        file3 = open(newReport,"w")
+        file3.write(txt1+'\n'+txt2)
+
+    def weight(self,name):
+        return os.stat(name).st_size        
