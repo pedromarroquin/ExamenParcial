c1569050adec0fe3267790f1cadd7cb778e22e6f
pedromarroquin <43348854+pedromarroquin@users.noreply.github.com>
2020-08-02 00:14:26 -0500

    Update report.py

--- report.py
+++ report.py
@@ -3,11 +3,16 @@ import platform
 import os
 
 
-
 class report:
      
     def __init__(self, nombre):
         self.nombre=nombre
+        archivox = open(nombre, "w")
+        archivox.close()
+
+
+    def setName(self):
+        return self.nombre
 
     def logging(self,captexto):
         archivo = open(self.nombre,"a")
@@ -21,7 +26,7 @@ class report:
             archivo.close()
         else:
             archivo = open(nombre,"w")
-            archivo.write("Tecla\t\t\t\tInicio\t\t\t\t\tFin\n")
+            archivo.write("Tecla\t\t\tInicio\t\t\t\t\tFin\n")
             archivo.close()
 
     def arch(self):
@@ -32,9 +37,6 @@ class report:
         elif so=='Linux':
             command="lscpu | head -24 | tee "+self.nombre
             os.system(command)
-        
-        
-        
     
     def joinFiles(self,file1,file2,newReport):
         txt1 = open(file1,"r").read()
@@ -44,4 +46,4 @@ class report:
         file3.write(txt1+'\n'+txt2)
 
     def weight(self,name):
-        return os.stat(name).st_size        
+        return os.stat(name).st_size   
